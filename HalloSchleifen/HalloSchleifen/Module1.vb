e8d773743e5aba796fd38292444da7a89ca325e4
André <AndreR@ppedv.de>
2024-04-11 13:51:54 +0200

    Schleifen und Arrays

---
+++ HalloSchleifen/HalloSchleifen/Module1.vb
@@ -0,0 +1,36 @@
+﻿Imports System.Reflection
+
+Module Module1
+
+    Sub Main()
+        Console.WriteLine("Hallo Schleifen")
+
+        Dim zahlen(4) As Integer
+        zahlen(0) = 12
+        zahlen(1) = -5
+        zahlen(2) = 3
+        zahlen(3) = 9999
+        zahlen(4) = -500
+
+        For index As Integer = 0 To zahlen.Length - 1
+            Console.WriteLine($"Der Wert von Index {index} ist {zahlen(index)}")
+        Next
+
+        For Each zahl As Integer In zahlen
+            Console.WriteLine($"Zahl: {zahl}")
+        Next
+
+
+        Dim eingabe As ConsoleKey
+
+        While eingabe <> ConsoleKey.D2
+
+
+            Console.WriteLine("Ende? [y]")
+            eingabe = Console.ReadKey().Key
+
+        End While
+
+    End Sub
+
+End Module
---
+++ HalloSchleifen/HalloSchleifen/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("HalloSchleifen")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("HalloSchleifen")>
+<Assembly: AssemblyCopyright("Copyright ©  2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("ce08dbca-8edc-4160-87ea-73d80ad6da82")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
