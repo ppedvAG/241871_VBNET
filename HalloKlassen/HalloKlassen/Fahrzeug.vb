d147f6f9f761e72f273284993db5e22d6b9eee3c
André <AndreR@ppedv.de>
2024-04-15 12:28:59 +0200

    Klassen

---
+++ HalloKlassen/HalloKlassen/Fahrzeug.vb
@@ -0,0 +1,57 @@
+﻿Public Class Fahrzeug
+    'felder
+    Private _farbe As String 'backing field
+    'Public hersteller As String
+    'Public modell As String
+
+    'properties
+    Public Property Farbe As String 'full Property
+        Get
+            Return _farbe
+        End Get
+        Set(value As String)
+            _farbe = value
+        End Set
+    End Property
+
+    Public Property Hersteller As String 'auto Property
+    Public Property Modell As String 'auto Property
+    Public ReadOnly Property Kennzeichen As String 'auto Property
+
+    Public Property AnzahlRäder As Integer 'auto Property
+
+
+    'subs / functions
+    Public Sub MachSignal()
+        Console.WriteLine($"Das {_farbe}e Fahrzeug macht BRRRRR")
+    End Sub
+
+    'konstruktor
+    Sub New(kennzeichen As String)
+        Farbe = "pink"
+        Me.Kennzeichen = kennzeichen
+    End Sub
+
+    Sub New(kennzeichen As String, farbe As String)
+        Me.Farbe = farbe
+        Me.Kennzeichen = kennzeichen
+    End Sub
+
+    Sub New(kennzeichen As String, räder As Integer)
+        Me.AnzahlRäder = räder
+        Me.Kennzeichen = kennzeichen
+    End Sub
+
+
+    'setter methode (unüblich!)
+    Public Sub SetFarbe(farbe As String)
+        _farbe = farbe
+        'Me == this
+    End Sub
+
+    'getter function (unüblich!)
+    Public Function GetFarbe() As String
+        Return _farbe
+    End Function
+
+End Class
---
+++ HalloKlassen/HalloKlassen/Module1.vb
@@ -0,0 +1,35 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("*** Hallo Klassen ***")
+
+        Dim meinAuto As Fahrzeug 'deklaration
+        meinAuto = New Fahrzeug("XY-QQ 666", "blau") 'instanzierung
+        meinAuto.Hersteller = "Baudi"
+        meinAuto.Modell = "911"
+        'meinAuto.Farbe = "blau"
+        'meinAuto.Kennzeichen = "XX-XX 888" 'geht nicht, weil readonly
+        'meinAuto.SetFarbe("blau")
+        meinAuto.MachSignal()
+        ZeigeFahrzeugInfos(meinAuto)
+
+        Dim meinFahrrad As Fahrzeug = New Fahrzeug("AB-CD 999", 8) 'deklaration + instanzierung
+        meinFahrrad.Hersteller = "Bose"
+        meinFahrrad.Modell = "Golf"
+        'meinFahrrad.Farbe = "gelb"
+        'meinFahrrad.SetFarbe("gelb")
+        meinFahrrad.MachSignal()
+        ZeigeFahrzeugInfos(meinFahrrad)
+
+        Console.WriteLine("Ende")
+        Console.ReadLine()
+
+    End Sub
+
+    Sub ZeigeFahrzeugInfos(fahrzeug As Fahrzeug)
+        Console.WriteLine($"Das Fahrzeug ist von Hersteller {fahrzeug.Hersteller}, " &
+                          $"das Modell {fahrzeug.Modell} in der Farbe {fahrzeug.Farbe}")
+    End Sub
+
+End Module
+
