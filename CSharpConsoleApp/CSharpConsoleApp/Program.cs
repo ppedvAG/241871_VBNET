f0dac1586d45b3b1d037db06570100be38d91c12
André <AndreR@ppedv.de>
2024-04-15 12:29:21 +0200

    C# App die VB Klassen konsumiert

---
+++ CSharpConsoleApp/CSharpConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using HalloKlassen;
+using System;
+
+
+namespace CSharpConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo Klasse C#");
+
+            var meinAuto = new Fahrzeug("HD-XX-1234");
+
+            meinAuto.MachSignal();
+
+
+            Console.ReadLine();
+        }
+    }
+}
